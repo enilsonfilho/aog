a631a64e07d1032a0589de89e45ff92d0624dcce
enilsonfilho <enilsonfilho.dev@hotmail.com>
2022-02-13 13:27:23 -0300

    Add title

--- lib/main.dart
+++ lib/main.dart
@@ -28,10 +28,25 @@ class HomePage extends StatelessWidget {
       backgroundColor: Theme.of(context).primaryColor,
       body: ListView(
         children: <Widget>[
+          const SizedBox(
+            height: 60,
+          ),
           Image.asset(
             "assets/images/aog-white.png",
             height: 80,
           ),
+          const SizedBox(
+            height: 10,
+          ),
+          const Text(
+            "Álcool ou Gasolina",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 25,
+              fontFamily: "Big Shoulders Display",
+            ),
+            textAlign: TextAlign.center,
+          )
         ],
       ),
     );
